f40e2f7bc30705929210036471585f0ce591b2f7
mlee2328 <mlee2@seattleu.edu>
2024-01-18 14:12:19 -0800

    added three hearts

--- Lib/GetOutOfDodge.cpp
+++ Lib/GetOutOfDodge.cpp
@@ -13,7 +13,9 @@ int		winWidth = 1523, winHeight = 472;
 
 Sprite	background;
 Sprite  sun;
-Sprite  heart;
+Sprite  heart1;
+Sprite heart2;
+Sprite heart3;
 string  heartImage = "Heart.png";
 string  sunImage = "Sun.png";
 string	scrollImage = "Clouds.png";
@@ -27,11 +29,11 @@ time_t	scrollTime = clock();
 void Scroll() {
 	time_t now = clock();
 	if (scrolling) {
-		float dt = (float)(now-scrollTime)/CLOCKS_PER_SEC;
-		(vertically? accumulatedVTime : accumulatedHTime) += dt;
+		float dt = (float)(now - scrollTime) / CLOCKS_PER_SEC;
+		(vertically ? accumulatedVTime : accumulatedHTime) += dt;
 	}
 	scrollTime = now;
-	float v = accumulatedVTime/loopDuration, u = accumulatedHTime/loopDuration;
+	float v = accumulatedVTime / loopDuration, u = accumulatedHTime / loopDuration;
 	background.uvTransform = Translate(u, v, 0);
 }
 
@@ -42,7 +44,9 @@ void Display() {
 	glDisable(GL_DEPTH_TEST);
 	background.Display();
 	sun.Display();
-	heart.Display();
+	heart1.Display();
+	heart2.Display();
+	heart3.Display();
 	glFlush();
 }
 
@@ -53,19 +57,35 @@ void Resize(int width, int height) {
 	sun.UpdateTransform();
 }
 
-int main(int ac, char **av) {
+void initializeHearts() {
+	heart1.compensateAspectRatio = true;
+	heart2.compensateAspectRatio = true;
+	heart3.compensateAspectRatio = true;
+
+	heart1.Initialize(heartImage, 0.9f);
+	heart1.SetScale(vec2(0.05f, 0.05f));
+	heart1.SetPosition(vec2(-0.97f, 0.9f));
+
+	heart2.Initialize(heartImage, 0.9f);
+	heart2.SetScale(vec2(0.05f, 0.05f));
+	heart2.SetPosition(vec2(-0.93f, 0.9f));
+
+	heart3.Initialize(heartImage, 0.9f);
+	heart3.SetScale(vec2(0.05f, 0.05f));
+	heart3.SetPosition(vec2(-0.89f, 0.9f));
+}
+
+int main(int ac, char** av) {
 	// init app window and GL context
-	GLFWwindow *w = InitGLFW(100, 100, winWidth, winHeight, "Dodge!!");
+	GLFWwindow* w = InitGLFW(100, 100, winWidth, winHeight, "Dodge!!");
 	background.Initialize(scrollImage, .7f);
 	sun.compensateAspectRatio = true;
-	heart.compensateAspectRatio = true;
+	//heart.compensateAspectRatio = true;
 	background.compensateAspectRatio = true;
 	sun.Initialize(sunImage, .8f);
 	sun.SetScale(vec2(0.3f, 0.3f));
 	sun.SetPosition(vec2(0.92f, 0.82f));
-	heart.Initialize(heartImage, 0.9f);
-	heart.SetScale(vec2(0.05f, 0.05f));
-	heart.SetPosition(vec2(-0.97f, 0.9f));
+	initializeHearts();
 	// callbacks
 	RegisterResize(Resize);
 	// event loop
