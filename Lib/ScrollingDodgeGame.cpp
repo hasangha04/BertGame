1ee32b2ceaef6cdb37157a0eac4cbde74b0782c0
mlee2328 <mlee2@seattleu.edu>
2024-01-23 14:05:07 -0800

    Added clock sprite

--- Lib/ScrollingDodgeGame.cpp
+++ Lib/ScrollingDodgeGame.cpp
@@ -1,4 +1,6 @@
-// GetOutOfDodge.cpp
+// ScrollingGame.cpp
+
+// 2D side-scrolling dodge game. Main character: Bert
 
 #include <glad.h>
 #include <GLFW/glfw3.h>
@@ -14,8 +16,10 @@ int		winWidth = 1522, winHeight = 790;
 Sprite	background;
 Sprite  sun;
 Sprite  heart1;
-Sprite heart2;
-Sprite heart3;
+Sprite  heart2;
+Sprite  heart3;
+Sprite  freezeClock;
+string  clockImage = "Clock.png";
 string  heartImage = "Heart.png";
 string  sunImage = "Sun.png";
 string	scrollImage = "Clouds.png";
@@ -47,6 +51,7 @@ void Display() {
 	heart1.Display();
 	heart2.Display();
 	heart3.Display();
+	//freezeClock.Display();
 	glFlush();
 }
 
@@ -79,12 +84,15 @@ int main(int ac, char** av) {
 	// init app window and GL context
 	GLFWwindow* w = InitGLFW(100, 100, winWidth, winHeight, "Dodge!!");
 	background.Initialize(scrollImage, .7f);
+	freezeClock.compensateAspectRatio = true;
 	sun.compensateAspectRatio = true;
 	background.compensateAspectRatio = true;
 	sun.Initialize(sunImage, .8f);
 	sun.SetScale(vec2(0.3f, 0.28f));
 	sun.SetPosition(vec2(0.92f, 0.82f));
 	initializeHearts();
+	freezeClock.Initialize(clockImage, 0.9f);
+	freezeClock.SetScale(vec2(0.075f, 0.075f));
 	// callbacks
 	RegisterResize(Resize);
 	// event loop
