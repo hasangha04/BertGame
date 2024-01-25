149c3af7f289467bef5ae0be715c18414f57ee4a
Harpreet Sangha <hasangha04@gmail.com>
2024-01-25 14:47:22 -0800

    Positioned the ground

--- Lib/ScrollingDodgeGame.cpp
+++ Lib/ScrollingDodgeGame.cpp
@@ -73,7 +73,7 @@ void Display() {
 	heart3.Display();
 	//freezeClock.Display();
 	//bertNeutral.Display();
-	//ground.Display();
+	ground.Display();
 	glFlush();
 }
 
@@ -110,6 +110,7 @@ int main(int ac, char** av) {
 	sun.compensateAspectRatio = true;
 	clouds.compensateAspectRatio = true;
 	bertNeutral.compensateAspectRatio = true;
+	ground.compensateAspectRatio = true;
 	sun.Initialize(sunImage, .8f);
 	sun.SetScale(vec2(0.3f, 0.28f));
 	sun.SetPosition(vec2(0.92f, 0.82f));
@@ -117,7 +118,9 @@ int main(int ac, char** av) {
 	freezeClock.Initialize(clockImage, 0.9f);
 	freezeClock.SetScale(vec2(0.075f, 0.075f));
 	bertNeutral.Initialize(bertNeutralImage, 0.9f);
-	//ground.Initialize(groundImage, 0.9f);
+	ground.Initialize(groundImage, 0.9f);
+	ground.SetScale(vec2(2.0f, 0.25f));
+	ground.SetPosition(vec2(0.0f, -0.75f));
 	// callbacks
 	RegisterResize(Resize);
 	// event loop
