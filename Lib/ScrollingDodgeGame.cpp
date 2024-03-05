ed60cfb59b3c4433ddc3ce35997e7233b44014b0
mlee2328 <mlee2@seattleu.edu>
2024-03-05 14:24:14 -0800

    changes jumping down velocity

--- Lib/ScrollingDodgeGame.cpp
+++ Lib/ScrollingDodgeGame.cpp
@@ -46,7 +46,7 @@ bool	startedGame = false, scrolling = false, endGame = false;
 float	currentScore = 0.0, highScore = 0.0;
 bool	bertBlinking = false;
 bool	jumping = false;
-float	velocityUp = 0.3f, velocityDown = -0.0000001f, gravity = -0.05f;
+float	velocityUp = 0.3f, velocityDown = 0.0f, gravity = -0.05f;
 bool	bertHit = false, bertPrevHit = false, bertSwitch = false;
 
 // times
@@ -137,7 +137,7 @@ void jumpingBert() {
 		}
 		if (jumpHeight <= -0.395f) {
 			velocityUp = 0.3f;
-			velocityDown = -0.0000001f;
+			velocityDown = 0.0f;
 			maxJumpHeight = -0.395f;
 			bertRunning.autoAnimate = true;
 			bertRunning.SetPosition(vec2(-1.0f, -0.395f));
