0a3e4099318e1a7386a3886070beb5068b8fcb91
mlee2328 <mlee2@seattleu.edu>
2024-03-06 22:49:32 -0800

    adjusted random generation of obstacles

--- Lib/ScrollingDodgeGame.cpp
+++ Lib/ScrollingDodgeGame.cpp
@@ -17,7 +17,8 @@ float	aspectRatio = (float) winWidth/winHeight;
 
 // sprites
 Sprite	clouds, sun, heart, freezeClock, bertNeutral, bertRunning, bertDetermined,
-		bertDead, bertHurt, bertIdle, gameLogo, ground, cactus, fence, bush, gameOver;
+		bertDead, bertHurt, bertIdle, gameLogo, ground, cactus1, cactus2, cactus3, 
+		fence1, fence2, fence3, bush1, bush2, bush3, gameOver;
 
 // images
 string  gameImage = "GameOver.png", gameLogoImage = "bert-game-logo.png";
@@ -25,22 +26,25 @@ string  cactusImage = "Cactus.png";
 string  fenceImage = "Fence.png";
 string  groundImage = "Ground.png";
 string  bushImage = "Bush.png";
-string  bertNeutralImage = "Bert-neutral.png", bertRunningImage = "Bert.gif";
-string  bertRun1 = "Bert-run1-mia.png", bertRun2 = "Bert-run2-mia.png";
-string  bertDeterminedImage = "Bert-determined.png", bertDeadImage = "Bert-dead.png";
+string  bertDeadImage = "Bert-dead.png";
 string  clockImage = "Clock.png";
 string  heartImage = "Heart.png";
 string  sunImage = "Sun.png";
 string	cloudsImage = "Clouds.png";
 
 // animations
-vector<string> bertNames = { bertRun1, bertRun2 };
+vector<string> bertNames = {"Bert-run1-mia.png", "Bert-run2-mia.png"};
 vector<string> bertHurts = {"Bert-determined-0.png", "Bert-determined-1.png", "Bert-determined-2.png"};
 vector<string> bertIdles = {"Bert-idle_0.png", "Bert-idle_1.png","Bert-idle_2.png", "Bert-idle_3.png", "Bert-idle_4.png"};
 
 // hearts
 int		numHearts = 3;
 vec2	heartPositions[] = { {-1.86f, 0.9f}, {-1.75f, 0.9f}, {-1.64f, 0.9f} };
+
+// obstacles
+Sprite cacti[3] = {cactus1, cactus2, cactus3};
+Sprite bushes[3] = {bush1, bush2, bush3};
+Sprite fences[3] = {fence1, fence2, fence3};
 
 // gamestate
 bool	startedGame = false, scrolling = false, endGame = false;
@@ -54,17 +58,19 @@ int 	level = 1;
 int		levelBound = 0;
 int		chance = 0;
 int     bounds[4];
+int		levels[] = {1, 0, 0};
+int numObstacles = 1;
 
 // times
 time_t	startTime = clock();
 time_t	bertIdleTime = clock(), bertBlinkTime = clock();
 time_t	scrollTimeClouds = clock(), scrollTimeGround = clock();
-time_t  startJump, endJump;
+time_t  startJump;
 time_t  bertHurtDisplayTime = 0;
 float   levelTime = 20.0; // Keeps track of how long a level is
 const float minLoopDurationGround = 1.5f;
 time_t spaceKeyDowntime = 0;
-float	loopDurationClouds = 60, loopDurationGround = 5;	// in seconds
+float	loopDurationClouds = 60, loopDurationGround = 3;	// in seconds
 
 // probes: locations wrt cactus sprite
 vec2	cactusSensors[] = { { .9f, .3f}, { .3f, 0.9f }, { -.2f, 0.9f }, { 0.9f, 0.0f }, { -.9f, -.4f }, { -.9f, .0f } };
@@ -98,36 +104,23 @@ void levelOutput()
 	bounds[0] = 25 - (5 * levelBound);
 	bounds[1] = 30 - (5 * levelBound);
 	bounds[2] = 45 + (5 * levelBound);
-	bounds[3] = 0 + (5 * levelBound);
 }
 
-int chanceOutput(int chance)
-{
-	if (scrolling)
-	{
+int chanceOutput(int chance) {
+	if (scrolling) {
 		// Level 1: Display cactus
-		if (chance >= 0 && chance < bounds[0])
-		{
+		if (chance >= 0 && chance < bounds[0]) {
 			return 1;
 		}
-		// Level 2: Display cactus and bush
-		else if (chance >= bounds[0] && chance < bounds[1])
-		{
+		// Level 2: Display bush
+		else if (chance >= bounds[0] && chance < bounds[1]) {
 			return 2;
 		}
-		// Level 3: Display cactus, bush, and fence
-		else if (chance >= bounds[1] && chance < bounds[2])
-		{
+		// Level 3: Display fence
+		else if (chance >= bounds[1] && chance < bounds[2]) {
 			return 3;
 		}
-		// Level 4: Display cactus, bush, and fence closer together
-		else if (chance >= bounds[2] && chance <= bounds[3])
-		{
-			return 4;
-		}
-		// Do the same as Level 1
-		else
-		{
+		else {
 			return 2;
 		}
 	}
@@ -147,7 +140,7 @@ void AdjustGroundLoopDuration()
 {
 	float elapsedTime = (float)(clock() - startTime) / CLOCKS_PER_SEC;
 
-	loopDurationGround = max(minLoopDurationGround, 5.0f - elapsedTime * 0.05f);
+	loopDurationGround = max(minLoopDurationGround, 3.0f - elapsedTime * 0.05f);
 }
 
 void ScrollGround() {
@@ -156,16 +149,15 @@ void ScrollGround() {
 		float dt = (float)(now - scrollTimeGround) / CLOCKS_PER_SEC;
 		float du = dt/loopDurationGround;
 		ground.uvTransform[0][3] += du;
-		// move sprites to keep pace with ground
-		cactus.position.x -= 2 * du * ground.scale.x * aspectRatio;
-		fence.position.x -= 2 * du * ground.scale.x * aspectRatio;
-		bush.position.x -= 2 * du * ground.scale.x * aspectRatio;
+		for (int i = 0; i < 3; i++) {
+			cacti[i].position.x -= 2 * du * ground.scale.x * aspectRatio;
+			fences[i].position.x -= 2 * du * ground.scale.x * aspectRatio;
+			bushes[i].position.x -= 2 * du * ground.scale.x * aspectRatio;
+			cacti[i].UpdateTransform();
+			fences[i].UpdateTransform();
+			bushes[i].UpdateTransform();
+		}
 		freezeClock.position.x -= 2 * du * ground.scale.x * aspectRatio;
-		// position in +/-1 space, but u is in 0,1, so scale by 2
-		// also scale by ground scale and aspect ratio
-		cactus.UpdateTransform();
-		fence.UpdateTransform();
-		bush.UpdateTransform();
 		freezeClock.UpdateTransform();
 	}
 	scrollTimeGround = now;
@@ -185,7 +177,6 @@ void jumpingBert() {
 		velocityUp += gravity * jumpTime;
 		bertRunning.SetPosition(vec2(-1.0f, maxJumpHeight));
 		bertHurt.SetPosition(vec2(-1.0f, maxJumpHeight));
-		endJump = clock();
 	}
 	else { 
 		if (jumping) {
@@ -208,80 +199,81 @@ void jumpingBert() {
 	}
 }
 
+// Generate Obstacles
+
+void GenerateObstacles(int numObstacles) {
+	float lastDistance = 0.0f;
+	cout << "New obstacles: " << endl;
+	for (int i = 0; i < numObstacles; i++) {
+		chance = 1 + (rand() % 100);
+		level = chanceOutput(chance);
+		levels[i] = level;
+		cout << level << endl;
+	}
+	for (int i = 0; i < numObstacles; i++) {
+		float minDistance = 5.0f * (1 / loopDurationGround);
+		float maxDistance = 8.0f * (1 / loopDurationGround);
+		float distanceRatio = (rand() % 100);
+		float distance = minDistance + (distanceRatio / 100) * (maxDistance - minDistance);
+		switch (levels[i]) {
+		case 1:
+			cacti[i].SetPosition(vec2(lastDistance + distance* aspectRatio, -.32f));
+			bertPrevHit = false;
+			bertSwitch = false;
+			loopedGround = false;
+			break;
+		case 2:
+			bushes[i].SetPosition(vec2(lastDistance + distance * aspectRatio, -.42f));
+			bertPrevHit = false;
+			bertSwitch = false;
+			loopedGround = false;
+			break;
+		case 3:
+			fences[i].SetPosition(vec2(3.0f + lastDistance + distance * aspectRatio, -.36f));
+			bertPrevHit = false;
+			bertSwitch = false;
+			loopedGround = false;
+			break;
+		}
+		lastDistance = distance;
+	}
+}
+
 // Gameplay
 
-void UpdateStatus() 
-{
+void UpdateStatus() {
 	if (!bertPrevHit && bertHit) {
 		bertPrevHit = true;
 		numHearts--;
 		endGame = numHearts <= 0;
 	}
 
-	vec2 c = cactus.position;
-	vec2 b = bush.position;
-	vec2 f = fence.position;
-
-	switch (level) 
-	{
-		case 1:
-			if (c.x < -1.0f * aspectRatio) 
-			{
-			}
-			else 
-			{
-				break;
-			}
-		case 2:
-			if (b.x >= -1.0f * aspectRatio)
-			{
-				break;
-			}
-		case 3:
-		case 4:
-			if (f.x >= -1.0f * aspectRatio)
-			{
-				break;
-			}
-			chance = 1 + (rand() % 100);
-			level = chanceOutput(chance);
-			loopedGround = true;
-			break;
+	vec2 lastObstacle;
+	if (levels[numObstacles - 1] == 1) {
+		lastObstacle = cacti[numObstacles - 1].position;
+	}
+	else if (levels[numObstacles - 1] == 2) {
+		lastObstacle = bushes[numObstacles - 1].position;
+	}
+	else if (levels[numObstacles - 1] == 3) {
+		lastObstacle = fences[numObstacles - 1].position;
 	}
 
-	if (loopedGround)
-	{
-		switch (level)
-		{
-			case 1:
-				cactus.SetPosition(vec2(1.2f * aspectRatio, -.32f));
-				bertPrevHit = false;
-				bertSwitch = false;
-				loopedGround = false;
-				break;
-			case 2:
-				cactus.SetPosition(vec2(1.2f * aspectRatio, -.32f));
-				bush.SetPosition(vec2(1.8f * aspectRatio, -.42f));
-				bertPrevHit = false;
-				bertSwitch = false;
-				loopedGround = false;
-				break;
-			case 3:
-				cactus.SetPosition(vec2(1.2f * aspectRatio, -.32f));
-				bush.SetPosition(vec2(1.8f * aspectRatio, -.42f));
-				fence.SetPosition(vec2(2.5f * aspectRatio, -.36f));
-				bertPrevHit = false;
-				bertSwitch = false;
-				loopedGround = false;
-				break;
-			case 4:
-				cactus.SetPosition(vec2(1.2f * aspectRatio, -.32f));
-				bush.SetPosition(vec2(1.7f * aspectRatio, -.42f));
-				fence.SetPosition(vec2(2.2f * aspectRatio, -.36f));
-				bertPrevHit = false;
-				bertSwitch = false;
-				loopedGround = false;
-				break;
+	if (lastObstacle.x <= -1.2f * aspectRatio) {
+		loopedGround = true;
+	}
+
+	if (loopedGround) {
+		if (loopDurationGround <= 2.0f) {
+			numObstacles = 1 + (rand() % 3);
+			GenerateObstacles(numObstacles);
+		}
+		else if (loopDurationGround <= 2.6f) {
+			numObstacles = 1 + (rand() % 2);
+			GenerateObstacles(numObstacles);
+		}
+		else {
+			GenerateObstacles(1);
 		}
 	}
 }
@@ -319,13 +311,13 @@ void blinkingBert() {
 	}
 }
 
-void displayCactus()
+void displayCactus(int index)
 {
 	for (int i = 0; i < nCactusSensors; i++)
 	{
-	    cactusProbes[i] = Probe(cactusSensors[i], cactus.ptTransform);
+	    //cactusProbes[i] = Probe(cactusSensors[i], cactus.ptTransform);
 	}
-	cactus.Display();
+	cacti[index].Display();
 	bertHit = false;
 	for (vec3 p : cactusProbes)
 	{
@@ -337,28 +329,13 @@ void displayCactus()
 	}
 }
 
-void displayCactusBush()
+void displayBush(int index)
 {
-	for (int i = 0; i < nCactusSensors; i++)
-	{
-		cactusProbes[i] = Probe(cactusSensors[i], cactus.ptTransform);
-	}
-	cactus.Display();
-	bertHit = false;
-	for (vec3 p : cactusProbes)
-	{
-		if (abs(p.z - bertRunning.z) < .05f)
-		{
-			bertHit = true;
-			bertSwitch = true;
-		}
-	}	
-
 	for (int i = 0; i < nBushSensors; i++)
 	{
-		bushProbes[i] = Probe(bushSensors[i], bush.ptTransform);
+		//bushProbes[i] = Probe(bushSensors[i], bush.ptTransform);
 	}
-	bush.Display();
+	bushes[index].Display();
 	for (vec3 u : bushProbes)
 	{
 		if (abs(u.z - bertRunning.z) < .05f)
@@ -369,42 +346,13 @@ void displayCactusBush()
 	}
 }
 
-void displayCactusBushFence()
+void displayFence(int index)
 {
-	for (int i = 0; i < nCactusSensors; i++)
-	{
-		cactusProbes[i] = Probe(cactusSensors[i], cactus.ptTransform);
-	}
-	cactus.Display();
-	bertHit = false;
-	for (vec3 p : cactusProbes)
-	{
-		if (abs(p.z - bertRunning.z) < .05f)
-		{
-			bertHit = true;
-			bertSwitch = true;
-		}
-	}
-
-	for (int i = 0; i < nBushSensors; i++)
-	{
-		bushProbes[i] = Probe(bushSensors[i], bush.ptTransform);
-	}
-	bush.Display();
-	for (vec3 u : bushProbes)
-	{
-		if (abs(u.z - bertRunning.z) < .05f)
-		{
-			bertHit = true;
-			bertSwitch = true;
-		}
-	}
-
 	for (int i = 0; i < nFenceSensors; i++)
 	{
-		fenceProbes[i] = Probe(fenceSensors[i], fence.ptTransform);
+		//fenceProbes[i] = Probe(fenceSensors[i], fence.ptTransform);
 	}
-	fence.Display();
+	fences[index].Display();
 	for (vec3 q : fenceProbes)
 	{
 		if (abs(q.z - bertRunning.z) < .05f)
@@ -417,7 +365,7 @@ void displayCactusBushFence()
 
 void Display(float dt) {
 	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	glEnable(GL_DEPTH_TEST);
 	glClear(GL_DEPTH_BUFFER_BIT);
 	clouds.Display();
@@ -458,21 +406,17 @@ void Display(float dt) {
 	}
 
 	//freezeClock.Display();
-	
-	switch (level)
-	{
-		case 1:
-			displayCactus();
-			break;
-		case 2:
-			displayCactusBush();
-			break;
-		case 3:
-			displayCactusBushFence();
-			break;
-		case 4:
-			displayCactusBushFence();
-			break;
+
+	for (int i = 0; i < 3; i++) {
+		if (levels[i] == 1) {
+			displayCactus(i);
+		}
+		else if (levels[i] == 2) {
+			displayBush(i);
+		}
+		else if (levels[i] == 3) {
+			displayFence(i);
+		}
 	}
 
 	if (endGame) {
@@ -539,8 +483,9 @@ void Keyboard(int key, bool press, bool shift, bool control) {
 		if (endGame) {
 			endGame = false;
 			numHearts = 3;
-			cactus.SetPosition(vec2(1.2f*aspectRatio, -0.32f));
-			fence.SetPosition(vec2(1.5f*aspectRatio, -0.36f));
+			/*cactus.SetPosition(vec2(1.2f * aspectRatio, -0.32f));
+			bush.SetPosition(vec2(1.2f * aspectRatio, -0.32f));
+			fence.SetPosition(vec2(1.5f*aspectRatio, -0.36f));*/
 			level = 1;
 			levelBound = 0;
 			levelTime = 20.0;
@@ -575,9 +520,11 @@ int main(int ac, char** av) {
 	initSprite(sun, sunImage, -.4f, {0.3f, 0.28f}, {1.77f, 0.82f});
 	initSprite(freezeClock, clockImage, -.4f, {0.12f, 0.12f}, {0.75f, -0.37f});
 	initSprite(ground, groundImage, -.4f, {2.0f, 0.25f}, {0.0f, -0.75f}, false);
-	initSprite(cactus, cactusImage, -.8f, {0.13f, 0.18f}, {2.5f, -0.32f});
-	initSprite(fence, fenceImage, -.8f, {0.35f, 0.15f}, {2.9f, -0.36f});
-	initSprite(bush, bushImage, -.8f, { 0.22f, 0.083f }, { 2.7f, -0.42f });
+	for (int i = 0; i < 3; i++) {
+		initSprite(cacti[i], cactusImage, -.8f, {0.13f, 0.18f}, {2.5f, -0.32f});
+		initSprite(fences[i], fenceImage, -.8f, {0.35f, 0.15f}, {2.9f, -0.36f});
+		initSprite(bushes[i], bushImage, -.8f, {0.22f, 0.083f}, {2.7f, -0.42f});
+	}
 	initSprite(gameOver, gameImage, -0.2f, {0.6f, 0.3f}, {0.0f, 0.0f});
 	initSprite(gameLogo, gameLogoImage, -0.2f, {0.6f, 0.3f}, {0.0f, 0.0f});
 	heart.Initialize(heartImage, -.4f);
